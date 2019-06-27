fe9ab6a1feb9cbf48345ed14647e75ba5165c09c
EmilJPedersen <34272937+EmilJPedersen@users.noreply.github.com>
2019-06-27 20:09:53 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { AboutusComponent } from './shared/aboutus/aboutus.component';
+import { CustommaterialModule } from './custommaterial.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutusComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CustommaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
